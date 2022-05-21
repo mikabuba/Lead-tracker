fefae42cfc1f332f76d8c85a17c9d91dc94720b8
mikabuba <105504032+mikabuba@users.noreply.github.com>
2022-05-21 14:20:47 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,84 @@
+export default {
+    products: [
+       {
+        _id: '1',
+        name: 'full zip running shirt',
+        category: 'shirt',
+        image: '/images/product-1.jpg',
+        price: 70,
+        brand: 'Hiskywin',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+
+       {
+        _id: '2',
+        name: 'Dinner wears',
+        category: 'shirt',
+        image: '/images/product-2.jpg',
+        price: 72,
+        brand: 'Hiskywin',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+
+       {
+        _id: '3',
+        name: 'full zip running shirt',
+        category: 'shirt',
+        image: '/images/product-3.jpg',
+        price: 165,
+        brand: 'Coporate wears',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+
+       {
+        _id: '4',
+        name: 'full zip running shirt',
+        category: 'shirt',
+        image: '/images/product-4.jpg',
+        price: 90,
+        brand: 'Hiskywin',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+
+    
+       {
+        _id: '5',
+        name: 'full zip running shirt',
+        category: 'shirt',
+        image: '/images/product-5.jpg',
+        price: 160,
+        brand: 'Hiskywin',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+
+    
+       {
+        _id: '6',
+        name: 'full zip running shirt',
+        category: 'shirt',
+        image: '/images/product-6.jpg',
+        price: 602,
+        brand: 'Hiskywin',
+        ratings: 4.5,
+        numReviews: 10,
+        countInstock: 6,
+  
+       },
+ 
+   ],
+};
